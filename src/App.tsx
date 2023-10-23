33f8c57339d0e8bba8f1d5c44f1e596468a1674e
kwakseongjae <gkffhdnls13@gmail.com>
2023-10-23 14:30:16 +0900

    CHORE: 빈폴더에 .gitkeep 파일 추가 & BrowserRouter를 main.tsx로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { BrowserRouter } from 'react-router-dom'
 import { RecoilRoot } from 'recoil'
 import Router from '@pages/Router'
 import GlobalStyle from '@styles/GlobalStyle'
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <GlobalStyle />
       <RecoilRoot>
         <Router />
       </RecoilRoot>
-    </BrowserRouter>
+    </>
   )
 }
 
