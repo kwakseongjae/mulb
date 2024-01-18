6104b23e00f2d16990590c24dd054bcd9cc3e6d7
kwakseongjae <gkffhdnls13@gmail.com>
2024-01-18 20:58:35 +0900

    styel: tailwind 적용 관련 수정사항 반영

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { RecoilRoot } from 'recoil'
+import '/dist/output.css'
 // import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 // import { QueryClient } from '@tanstack/react-query'
 // const queryClient = new QueryClient()
