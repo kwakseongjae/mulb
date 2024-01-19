779e0aa9db42173ebdf6b49b3a195706310580c1
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2024-01-19 21:40:21 +0900

    Update LatestBoard.tsx

--- src/components/Feature/LatestBoard.tsx
+++ src/components/Feature/LatestBoard.tsx
@@ -4,10 +4,7 @@ import { TextPostCard } from '@components/common/PostCards.tsx'
 
 function LatestBoard({ className }: Default): React.JSX.Element {
   return (
-    <section className={`${className}`}>
-      <h1 className="text-base font-bold border-t-2 border-b mb-8 pt-4 pb-2">
-        최근 포스트
-      </h1>
+    <section className={`border-t pt-4 ${className}`}>
       <div className="w-full flex items-center justify-center gap-8">
         <div className="w-1/4 flex-wrap">
           <TextPostCard response={PostCardData} />
