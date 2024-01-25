394a0756bb52a17b308cba98bc66e71706187553
kwakseongjae <gkffhdnls13@gmail.com>
2024-01-25 16:30:19 +0900

    rename: 포스팅 수정, 포스팅 상세 페이지 파일 위치 변경

---
+++ src/pages/posts/detail.tsx
@@ -0,0 +1,5 @@
+function PostDetail() {
+  return <div>개별 포스트 내용을 확인하는 페이지입니다.</div>
+}
+
+export default PostDetail
---
+++ src/pages/posts/edit.tsx
@@ -0,0 +1,3 @@
+export default function EditPost() {
+  return <div>게시물 수정 페이지 입니다.</div>
+}
